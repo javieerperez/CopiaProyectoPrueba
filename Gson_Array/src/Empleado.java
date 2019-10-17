b7e522124caad7aa5f6f27cde52b265d0e53626c
javie <javie@LAPTOP-19T0SJJI>
2019-10-17 23:29:03 +0200

    hola

--- Gson_Array/src/Empleado.java
+++ Gson_Array/src/Empleado.java
@@ -2,17 +2,17 @@
 public class Empleado {
 	
     private final int id;
-    private final String nombreEmpleado;
+    private final String nombre;
     private final String empresa;
  
-    public Empleado(int id, String nombreEmpleado, String empresa) {
+    public Empleado(int id, String nombre, String empresa) {
         this.id = id;
-        this.nombreEmpleado = nombreEmpleado;
+        this.nombre = nombre;
         this.empresa = empresa;
     }
     
     public String toString() {
-    	String salida = "Empleado: ( "+id+", "+nombreEmpleado+", "+empresa+" )";
+    	String salida = "Empleado: ( "+id+", "+nombre+", "+empresa+" )";
     	return salida;
     }
 }
